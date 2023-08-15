deb2fce4bc3c54b2396ddb25ea86ab571cb451de
Khlawy <mhmdalkhlawy45@gmail.com>
2023-08-15 12:04:13 +0300

    Added folder for Sheet #1 (Data type - Conditions), and solved A. Say Hello With C++

---
+++ Sheet #1 (Data type - Conditions)/Main Folder/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Main_Folder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
