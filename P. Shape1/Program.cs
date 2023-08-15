bd736ca8813c5f7b6123c7d8d4411dd9d93b575a
Khlawy <mhmdalkhlawy45@gmail.com>
2023-08-15 11:22:44 +0300

    2 Problems: O. Pyramid, P. Shape1

---
+++ P. Shape1/Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int number = int.Parse(Console.ReadLine()!);
+
+        for (int i = number; i > 0; i--)
+        {
+            for (int j = i; j > 0; j--)
+            {
+                Console.Write('*');
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Problem Solving/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Numerics;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        
+    }
+}
