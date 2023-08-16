cd4efb0bc81ce0503c819cd44692c0d837d4a591
Khlawy <mhmdalkhlawy45@gmail.com>
2023-08-16 12:27:55 +0300

    Add new folder for Problemset in Codeforces

---
+++ Problemset/A. Ilya and Bank Account/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace A._Ilya_and_Bank_Account
+{
+    internal class Program
+    {
+        // Problem Link: https://codeforces.com/problemset/problem/313/A
+        static void Main(string[] args)
+        {
+            string number = Console.ReadLine()!;
+
+            if (int.Parse(number) > 0)
+            {
+                Console.WriteLine(number);
+            }
+            else
+            {
+                string removeLast = number.Substring(0, number.Length - 1);
+
+                string removeBeforeLast = number.Substring(0, number.Length - 2);
+                removeBeforeLast += number[number.Length - 1];
+
+                Console.WriteLine(Math.Max(int.Parse(removeBeforeLast), int.Parse(removeLast)));
+            }
+        }
+    }
+}
---
+++ Problemset/Problemset_MainFolder/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Problemset_MainFolder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
