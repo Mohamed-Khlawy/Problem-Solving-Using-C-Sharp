26b37a64ce5499af5f2a2493c40978fcf2398da2
Khlawy <mhmdalkhlawy45@gmail.com>
2023-08-17 12:27:56 +0300

    I solved these: R. Sequence of Numbers and Sum/ S. Sum of Consecutive Odd Numbers

---
+++ Sheet #2 (Loops)/R. Sequence of Numbers and Sum/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace R._Sequence_of_Numbers_and_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string numbers = Console.ReadLine()!;
+                string[] inputs = numbers.Split(' ');
+
+                int number1 = int.Parse(inputs[0]);
+                int number2 = int.Parse(inputs[1]);
+                if(number1 <= 0 || number2 <= 0)
+                {
+                    break;
+                }
+
+                int sum = 0;
+
+                for (int i = Math.Min(number1, number2); i <= Math.Max(number1, number2); i++)
+                {
+                    Console.Write(i + " ");
+                    sum += i;
+                }
+                Console.Write($"sum ={sum}");
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Sheet #2 (Loops)/S. Sum of Consecutive Odd Numbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Numerics;
+
+namespace S._Sum_of_Consecutive_Odd_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int tests = int.Parse(Console.ReadLine()!);
+            while(tests--  > 0)
+            {
+                string numbers = Console.ReadLine()!;
+                string[] inputs = numbers.Split(' ');
+
+                int number1 = Math.Min(int.Parse(inputs[0]), int.Parse(inputs[1]));
+                int number2 = Math.Max(int.Parse(inputs[0]), int.Parse(inputs[1]));
+                BigInteger sum = 0;
+
+                for (int i = number1 + 1; i < number2; i++)
+                {
+                    if (i % 2 != 0)
+                    {
+                        sum += i;
+                    }
+                }
+
+                Console.WriteLine(sum);
+            }
+        }
+    }
+}
