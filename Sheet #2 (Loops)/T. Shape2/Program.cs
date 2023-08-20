ffcca66e72499361fe46c0d4691fe9778f7ee215
Khlawy <mhmdalkhlawy45@gmail.com>
2023-08-20 12:25:50 +0300

    Solved these from sheet 2: T. Shape2/ U. Some Sums

---
+++ Sheet #2 (Loops)/T. Shape2/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace T._Shape2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine()!);
+            int stars = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                // Spaces Loop
+                for (int j = n; j > i; j--)
+                {
+                    Console.Write(' ');
+                }
+
+                // Stars Loop
+                for (int j = 1; j <= stars; j++)
+                {
+                    Console.Write('*');
+                }
+                // Increment stars with 2 each iteration
+                stars += 2;
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Sheet #2 (Loops)/U. Some Sums/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Numerics;
+
+namespace U._Some_Sums
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine()!;
+            string[] elements = input.Split(' ');
+
+            int n = int.Parse(elements[0].ToString());
+            int a = int.Parse(elements[1].ToString());
+            int b = int.Parse(elements[2].ToString());
+            BigInteger sum = 0;
+
+            while (n > 0)
+            {
+                int copy_n = n;
+                int digitsSum = 0;
+
+                while (copy_n > 0)
+                {
+                    digitsSum += copy_n % 10;
+                    copy_n /= 10;
+                }
+
+                if (digitsSum >= a && digitsSum <= b)
+                {
+                    sum += n;
+                }
+
+                n--;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
