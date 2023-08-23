f25e5aac14c73539257eccd9d2b6ea1e1896c39c
Khlawy <mhmdalkhlawy45@gmail.com>
2023-08-23 21:23:07 +0300

    Added a file with links of solved problems, and solved these : A. Insomnia cure \\ B. Even Array

---
+++ Problemset/A. Insomnia cure/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace A._Insomnia_cure
+{
+    internal class Program
+    {
+        // Problem Link: https://codeforces.com/problemset/problem/148/A
+        static void Main(string[] args)
+        {
+            int k_Dragons = int.Parse(Console.ReadLine()!);
+            int l_Dragons = int.Parse(Console.ReadLine()!);
+            int m_Dragons = int.Parse(Console.ReadLine()!);
+            int n_Dragons = int.Parse(Console.ReadLine()!);
+            int totalDragons = int.Parse(Console.ReadLine()!);
+
+            int totalDamaged = 0;
+            for (int i = 1; i <= totalDragons; i++)
+            {
+                if (i % k_Dragons == 0 || i % l_Dragons == 0 || i % m_Dragons == 0 || i % n_Dragons == 0)
+                {
+                    totalDamaged++;
+                }
+            }
+
+            Console.WriteLine(totalDamaged);
+        }
+    }
+}
---
+++ Problemset/B. Even Array/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace B._Even_Array
+{
+    internal class Program
+    {
+        // Problem Link: https://codeforces.com/problemset/problem/1367/B
+        static void Main(string[] args)
+        {
+            int tests = int.Parse(Console.ReadLine()!);
+            while (tests-- > 0)
+            {
+                int arrayLength = int.Parse(Console.ReadLine()!);
+                string arrayNumbers = Console.ReadLine()!;
+                string[] split_arrayNumbers = arrayNumbers.Split(' ');
+
+                int[] array = new int[arrayLength];
+                List<int> swapsArray = new List<int>();
+
+                int evenCounter = 0;
+                int oddCounter = 0;
+
+                for (int i = 0; i < arrayLength; i++)
+                {
+                    array[i] = int.Parse(split_arrayNumbers[i]);
+                    if (array[i] % 2 != i % 2)
+                    {
+                        swapsArray.Add(array[i]);
+                    }
+                }
+
+                for (int i = 0; i < swapsArray.Count; i++)
+                {
+                    if (swapsArray[i] % 2 == 0)
+                    {
+                        evenCounter++;
+                    }
+                    else
+                    {
+                        oddCounter++;
+                    }
+                }
+
+                if (evenCounter == oddCounter)
+                {
+                    Console.WriteLine(evenCounter);//or odd counter, they will be same in this case
+                }
+                else
+                {
+                    Console.WriteLine(-1);
+                }
+            }
+        }
+    }
+}
