88e2a5d6d2aeb964bf24f58514581b89cd14efbe
Khlawy <mhmdalkhlawy45@gmail.com>
2023-08-23 12:38:03 +0300

    Solved this from Problemset : A. Even Odds

---
+++ Problemset/A. Even Odds/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Numerics;
+
+namespace A._Even_Odds
+{
+    internal class Program
+    {
+        // Problem Link: https://codeforces.com/problemset/problem/318/A
+        // This problem wath hard on me
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine()!;
+            string[] inputArray = input.Split(' ');
+
+            BigInteger lastNumber = BigInteger.Parse(inputArray[0]);
+            BigInteger indexOfNumber = BigInteger.Parse(inputArray[1]);
+
+            BigInteger half = (lastNumber + 1) / 2;
+
+            if (indexOfNumber <= half)//The number is in odd half
+            {
+                Console.WriteLine((2 * indexOfNumber) - 1);
+            }
+            else//The number is in even half
+            {
+                Console.WriteLine(2 * (indexOfNumber - half));
+            }
+        }
+    }
+}
