9fe67b766e411a763b74ddf80a0af21bc80bbd36
Khlawy <mhmdalkhlawy45@gmail.com>
2023-08-24 13:29:42 +0300

    Solved from Contest #2 : D. Range Sum

---
+++ Contest #2/D. Range Sum/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Numerics;
+
+namespace D._Range_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int tests = int.Parse(Console.ReadLine()!);
+            while(tests-- > 0)
+            {
+                string l_r = Console.ReadLine()!;
+                string[] l_rArray = l_r.Split(' ');
+
+                int left = Math.Min(int.Parse(l_rArray[0]), int.Parse(l_rArray[1]));
+                int right = Math.Max(int.Parse(l_rArray[0]), int.Parse(l_rArray[1]));
+                long numbersInRamge = right - left + 1;
+
+                long sum = (numbersInRamge * (left + right)) / 2;
+
+                Console.WriteLine(sum);
+            }
+        }
+    }
+}
