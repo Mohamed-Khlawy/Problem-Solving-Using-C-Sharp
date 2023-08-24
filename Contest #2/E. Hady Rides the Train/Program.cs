ab9e501c6a9074ddd0d0414c0db08f16bbf7ddd4
Khlawy <mhmdalkhlawy45@gmail.com>
2023-08-24 22:19:37 +0300

    Solved from Contest #2 : E. Hady Rides the Train

---
+++ Contest #2/E. Hady Rides the Train/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace E._Hady_Rides_the_Train
+{
+    internal class Program
+    {
+        // This was hard but i got it alone
+        static void Main(string[] args)
+        {
+            long seatIndex = long.Parse(Console.ReadLine()!);
+            long rowIndex = seatIndex / 4;
+            long columnIndex;
+
+            if (rowIndex % 2 == 0)
+            {
+                columnIndex = seatIndex % 4;
+            }
+            else
+            {
+                columnIndex = 3 - (seatIndex % 4);
+            }
+
+            Console.WriteLine(rowIndex.ToString("F0") + " " + columnIndex);
+        }
+    }
+}
