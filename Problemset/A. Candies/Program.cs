dac8b93e150b42528dcffcaee94d7a1c81f17a83
Khlawy <mhmdalkhlawy45@gmail.com>
2023-08-27 20:41:18 +0300

    Solved from problem problemset : A. Candies

---
+++ Problemset/A. Candies/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace A._Candies
+{
+    internal class Program
+    {
+        // Problem link: https://codeforces.com/problemset/problem/1343/A
+        // This problem was hard on me, and i saw it's tutorial
+        static void Main(string[] args)
+        {
+            int tests = int.Parse(Console.ReadLine()!);
+
+            while (tests-- > 0)
+            {
+                int candyWrappers = int.Parse(Console.ReadLine()!);
+
+                for (int k_day = 2; k_day < 30; k_day++)
+                {
+
+                    if (candyWrappers % (Math.Pow(2, k_day) - 1) == 0)
+                    {
+                        double numberOfCandies = candyWrappers / (Math.Pow(2, k_day) - 1);
+                        Console.WriteLine(numberOfCandies);
+                        break;
+                    }
+                }
+
+            }
+        }
+    }
+}
