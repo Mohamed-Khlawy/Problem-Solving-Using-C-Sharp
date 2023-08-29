240d1fa250ff239dcf4fa8c675995afc6fa5fbd0
Khlawy <mhmdalkhlawy45@gmail.com>
2023-08-29 12:21:39 +0300

    Solved from Sheet #3 : A. Summation \\ B. Searching

---
+++ Sheet #3 (Arrays)/A. Summation/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace A._Summation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine()!);
+            int[] numbersArray = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+            long sum = 0;
+
+            for(int i = 0; i < numbersArray.Length; i++)
+            {
+                if (numbersArray[i] > 0)
+                {
+                    sum += numbersArray[i];
+                }
+                else if (numbersArray[i] < 0)
+                {
+                    sum += numbersArray[i];
+                }
+            }
+
+            Console.WriteLine(Math.Abs(sum));
+        }
+    }
+}
---
+++ Sheet #3 (Arrays)/B. Searching/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace B._Searching
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine()!);
+            int[] numbersArray = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+            int numberForSearch = int.Parse(Console.ReadLine()!);
+
+            if (numbersArray.Contains(numberForSearch))
+            {
+                for(int i = 0; i < numbersArray.Length; i++)
+                {
+                    if (numbersArray[i] == numberForSearch)
+                    {
+                        Console.WriteLine(i);
+                        return;
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine(-1);
+            }
+        }
+    }
+}
