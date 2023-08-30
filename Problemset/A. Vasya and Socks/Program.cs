31e32e7528efa8aa4d666b925d6f02d63ac2e79b
Khlawy <mhmdalkhlawy45@gmail.com>
2023-08-30 12:36:36 +0300

    Solved from Problemset : A. Vasya and Socks

---
+++ Problemset/A. Vasya and Socks/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace A._Vasya_and_Socks
+{
+    internal class Program
+    {
+        //Problem Link: https://codeforces.com/problemset/problem/460/A
+        static void Main(string[] args)
+        {
+            string[] n_m = Console.ReadLine()!.Split().ToArray();
+            int n = int.Parse(n_m[0]);
+            int m = int.Parse(n_m[1]);
+            int copy_n = n;
+
+            int newPairs = 0;
+
+            while (copy_n / m != newPairs)
+            {
+                newPairs = copy_n / m;
+                copy_n = n + newPairs;
+            }
+
+            Console.WriteLine(copy_n);
+        }
+    }
+}
