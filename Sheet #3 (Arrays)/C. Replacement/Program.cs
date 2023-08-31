be6d887df9a3792227fd61b75a7b9cf82a7e7b76
Khlawy <mhmdalkhlawy45@gmail.com>
2023-08-31 12:22:39 +0300

    Solved from Sheet #3 : C. Replacement \\ D. Positions in array

---
+++ Sheet #3 (Arrays)/C. Replacement/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace C._Replacement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine()!);
+            int[] numbersArray = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+
+            for (int i = 0; i < numbersArray.Length; i++)
+            {
+                numbersArray[i] = numbersArray[i] switch
+                {
+                    > 0 => 1,
+                    < 0 => 2,
+                    _ => 0,
+                };
+
+                //switch (numbersArray[i])  // Another way for switch statements
+                //{
+                //    case > 0:
+                //        numbersArray[i] = 1;
+                //        break;
+                //    case < 0:
+                //        numbersArray[i] = 2;
+                //        break;
+                //    default:
+                //        numbersArray[i] = 0;
+                //        break;
+                //}
+            }
+
+            for (int i = 0; i < numbersArray.Length; i++)
+            {
+                Console.Write(numbersArray[i] + " ");
+            }
+        }
+    }
+}
---
+++ Sheet #3 (Arrays)/D. Positions in array/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace D._Positions_in_array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine()!);
+            int[] numbersArray = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+
+            for (int i = 0; i < numbersArray.Length; i++)
+            {
+                if (numbersArray[i] <= 10)
+                {
+                    Console.WriteLine($"A[{i}] = {numbersArray[i]}");
+                }
+            }
+        }
+    }
+}
