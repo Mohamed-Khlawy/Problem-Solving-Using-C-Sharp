c79650f0db4af0c7f3513009e48be4f3d70df9de
Khlawy <mhmdalkhlawy45@gmail.com>
2023-09-02 21:31:42 +0300

    Solved from Problemset : A. New Year Candles \\ B. Multiply by 2, divide by 6

---
+++ Problemset/A. New Year Candles/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace A._New_Year_Candles
+{
+    internal class Program
+    {
+        //Problem Link: https://codeforces.com/problemset/problem/379/A
+        static void Main(string[] args)
+        {
+            int[] numberOfCandles_wentToNew = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+            int numberOfCandles = numberOfCandles_wentToNew[0];
+            int wentToNew = numberOfCandles_wentToNew[1];
+            int copy_numberOfCandles = numberOfCandles;
+
+            int newCandles = 0;
+            while (copy_numberOfCandles / wentToNew != newCandles)
+            {
+                newCandles = copy_numberOfCandles / wentToNew;
+                copy_numberOfCandles = numberOfCandles + newCandles;
+            }
+
+            Console.WriteLine(copy_numberOfCandles);
+        }
+    }
+}
---
+++ Problemset/B. Multiply by 2, divide by 6/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace B._Multiply_by_2__divide_by_6
+{
+    internal class Program
+    {
+        //Problem Link: https://codeforces.com/problemset/problem/1374/B
+        //This was hard and i saw it's Tutorial
+        static void Main(string[] args)
+        {
+            int tests = int.Parse(Console.ReadLine()!);
+            while (tests-- > 0)
+            {
+                int number = int.Parse(Console.ReadLine()!);
+                int copy_number = number;
+                int counterOf2 = 0;
+                int counterOf3 = 0;
+
+                while (copy_number % 2 == 0)
+                {
+                    copy_number /= 2;
+                    counterOf2++;
+                }
+                while (copy_number % 3 == 0)
+                {
+                    copy_number /= 3;
+                    counterOf3++;
+                }
+
+                if (copy_number == 1 && counterOf2 <= counterOf3)
+                {
+                    Console.WriteLine(2 * counterOf3 - counterOf2);
+                }
+                else
+                {
+                    Console.WriteLine(-1);
+                }
+            }
+        }
+    }
+}
