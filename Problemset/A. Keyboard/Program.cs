5e6ccfc8508cede04a69e2df7f60a6626976cd13
Khlawy <mhmdalkhlawy45@gmail.com>
2023-09-04 21:47:23 +0300

    Solved from Problemset : A. Olesya and Rodion \\ A. Keyboard

---
+++ Problemset/A. Keyboard/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace A._Keyboard
+{
+    internal class Program
+    {
+        //Problem Link: https://codeforces.com/problemset/problem/474/A
+        static void Main(string[] args)
+        {
+            string direction = Console.ReadLine()!;
+            string wrongWord = Console.ReadLine()!;
+            string realWord = "";
+            string keyboardChars = "qwertyuiopasdfghjkl;zxcvbnm,./";
+
+            if (direction == "R") //His hands move Right, so we will get the (previous) char 
+            {
+                for (int i = 0; i < wrongWord.Length; i++)
+                {
+                    int realCharIndex = keyboardChars.IndexOf(wrongWord[i]) - 1;
+                    realWord += keyboardChars[realCharIndex];
+                }
+
+                Console.WriteLine(realWord);
+            }
+
+            else if (direction == "L") //His hands move Left, so we will get the (next) char
+            {
+                for (int i = 0; i < wrongWord.Length; i++)
+                {
+                    int realCharIndex = keyboardChars.IndexOf(wrongWord[i]) + 1;
+                    realWord += keyboardChars[realCharIndex];
+                }
+
+                Console.WriteLine(realWord);
+            }
+        }
+    }
+}
---
+++ Problemset/A. Olesya and Rodion/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace A._Olesya_and_Rodion
+{
+    internal class Program
+    {
+        //Problem Link: https://codeforces.com/problemset/problem/584/A
+        static void Main(string[] args)
+        {
+            int[] Olesya_Rodion = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+            int Olesya = int.Parse(Olesya_Rodion[0].ToString());
+            int Rodion = int.Parse(Olesya_Rodion[1].ToString());
+            string solution = "";
+
+            if (Rodion == 10)
+            {
+                if (Olesya == 1)
+                {
+                    Console.WriteLine(-1);
+                    return;
+                }
+
+                else
+                {
+                    solution += 1;
+
+                    for (int i = 0; i < Olesya - 1; i++)
+                    {
+                        solution += 0;
+                    }
+
+                    Console.WriteLine(solution);
+                }
+            }
+
+            else
+            {
+                for (int i = 0; i < Olesya; i++)
+                {
+                    solution += Rodion;
+                }
+
+                Console.WriteLine(solution);
+            }
+        }
+    }
+}
