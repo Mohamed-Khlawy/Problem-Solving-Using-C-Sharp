4860e00a93db04f5ea1d4012b96ca52402467ef7
Khlawy <mhmdalkhlawy45@gmail.com>
2023-09-06 20:20:49 +0300

    Solved from Problemset : A. Dreamoon and Stairs \\ A. To My Critics

---
+++ Problemset/A. Dreamoon and Stairs/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace A._Dreamoon_and_Stairs
+{
+    internal class Program
+    {
+        //Problem Link: https://codeforces.com/problemset/problem/476/A
+        static void Main(string[] args)
+        {
+            int[] steps_number = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+            double steps = steps_number[0];
+            double number = steps_number[1];
+
+            double minMoves = Math.Ceiling(steps / 2);
+
+            while (minMoves % number != 0 && minMoves <= steps)
+            {
+                minMoves++;
+            }
+
+            if (minMoves > steps)
+            {
+                Console.WriteLine(-1);
+            }
+            else
+            {
+                Console.WriteLine(minMoves);
+            }
+        }
+    }
+}
---
+++ Problemset/A. To My Critics/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace A._To_My_Critics
+{
+    internal class Program
+    {
+        //Problem Link: https://codeforces.com/problemset/problem/1850/A
+        static void Main(string[] args)
+        {
+            int tests = int.Parse(Console.ReadLine()!);
+            while (tests-- > 0)
+            {
+                int[] numbersArray = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+                bool found = false;
+
+                for (int i = 0; i < numbersArray.Length; i++)
+                {
+                    int j = 0;
+                    while (j < numbersArray.Length)
+                    {
+                        if (j != i)
+                        {
+                            int sum = numbersArray[j] + numbersArray[i];
+                            if (sum >= 10)
+                            {
+                                found = true;
+                                break;
+                            }
+                        }
+
+                        j++;
+                    }
+                }
+
+                if (found)
+                {
+                    Console.WriteLine("YES");
+                }
+                else
+                {
+                    Console.WriteLine("NO");
+                }
+            }
+        }
+    }
+}
