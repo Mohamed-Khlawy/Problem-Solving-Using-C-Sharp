c47251c6beb3b175bc49556d5963e5a909106975
Khlawy <mhmdalkhlawy45@gmail.com>
2023-09-08 20:10:14 +0300

    Solved from Sheet #3: P. Minimize Number \\ Q. Count Subarrays

---
+++ Sheet #3 (Arrays)/P. Minimize Number/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace P._Minimize_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine()!);
+            int[] numbersArray = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+            bool foundOdd = false;
+            int dividesCounter = 0;
+
+            while (true)
+            {
+                for (int i = 0; i < numbersArray.Length; i++)
+                {
+                    if (numbersArray[i] % 2 == 0)
+                    {
+                        numbersArray[i] /= 2;
+                    }
+                    else
+                    {
+                        foundOdd = true;
+                        break;
+                    }
+                }
+
+                if (foundOdd)
+                {
+                    break;
+                }
+                else
+                {
+                    dividesCounter++;
+                }
+            }
+
+            Console.WriteLine(dividesCounter);
+        }
+    }
+}
---
+++ Sheet #3 (Arrays)/Q. Count Subarrays/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Q._Count_Subarrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int tests = int.Parse(Console.ReadLine()!);
+            while (tests-- > 0)
+            {
+                int size = int.Parse(Console.ReadLine()!);
+                int[] numbersArray = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+                int subArraysCounter = 0;
+
+                for (int i = 0; i < numbersArray.Length; i++)
+                {
+                    int lastNumber = numbersArray[i];
+                    for (int j = i; j < numbersArray.Length; j++)
+                    {
+                        if (numbersArray[j] < lastNumber)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            subArraysCounter++;
+                            lastNumber = numbersArray[j];
+                        }
+                    }
+                }
+
+                Console.WriteLine(subArraysCounter);
+            }
+        }
+    }
+}
