11304260487d894baf3ed46e88a351e06eb5b10f
Khlawy <mhmdalkhlawy45@gmail.com>
2023-09-09 22:31:56 +0300

    Solved from Problemset : A. Array with Odd Sum \\ A. Case of the Zeros and Ones

---
+++ Problemset/A. Array with Odd Sum/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace A._Array_with_Odd_Sum
+{
+    internal class Program
+    {
+        //Problem Link: https://codeforces.com/problemset/problem/1296/A
+        // It was hard and i saw it's Tutorial
+        static void Main(string[] args)
+        {
+            int tests = int.Parse(Console.ReadLine()!);
+            while (tests-- > 0)
+            {
+                int size = int.Parse(Console.ReadLine()!);
+                int[] numbersArray = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+
+                long arraySum = 0;
+                bool foundOdd = false;
+                bool foundEven = false;
+
+                for (int i = 0; i < numbersArray.Length; i++)
+                {
+                    arraySum += numbersArray[i];
+
+                    if (numbersArray[i] % 2 != 0)
+                    {
+                        foundOdd = true;
+                    }
+                    else
+                    {
+                        foundEven = true;
+                    }
+                }
+
+                if (arraySum % 2 != 0 || (foundEven && foundOdd))
+                {
+                    Console.WriteLine("YES");
+                }
+                else
+                {
+                    Console.WriteLine("NO");
+                }
+            }
+        }
+    }
+}
---
+++ Problemset/A. Case of the Zeros and Ones/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace A._Case_of_the_Zeros_and_Ones
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine()!);
+            string inputString = Console.ReadLine()!;
+
+            List<int> number = new List<int>();
+
+            for (int i = 0; i < size; i++)
+            {
+                number.Add(int.Parse(inputString[i].ToString()));
+            }
+
+            long counterForOne = 0;
+            long counterForZero = 0;
+
+            foreach (int element in number)
+            {
+                if (element == 1)
+                {
+                    counterForOne++;
+                }
+                else if (element == 0)
+                {
+                    counterForZero++;
+                }
+            }
+
+            Console.WriteLine(Math.Abs(counterForZero - counterForOne));
+        }
+    }
+}
