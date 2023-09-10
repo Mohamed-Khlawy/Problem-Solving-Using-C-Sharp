aea32096dfce2bf27132ac0dab1e5d9d207302fc
Khlawy <mhmdalkhlawy45@gmail.com>
2023-09-10 20:10:35 +0300

    Solved from Sheet #3 : R. Permutation with arrays \\ S. Search In Matrix

---
+++ Sheet #3 (Arrays)/R. Permutation with arrays/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace R._Permutation_with_arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine()!);
+            int[] firstArray = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+            int[] secondArray = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+
+            Array.Sort(firstArray);
+            Array.Sort(secondArray);
+            bool notPermutation = false;
+
+            for (int i = 0; i < size; i++)
+            {
+                if (firstArray[i] != secondArray[i])
+                {
+                    notPermutation = true;
+                    break;
+                }
+            }
+
+            if (!notPermutation)
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
---
+++ Sheet #3 (Arrays)/S. Search In Matrix/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace S._Search_In_Matrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] row_col = Console.ReadLine()!.Split().Select(int.Parse).ToArray();// User input
+            int row = row_col[0];
+            int col = row_col[1];
+            int[,] matrix = new int[row, col];
+
+            for (int i = 0; i < row; i++)
+            {
+                int[] rowElements = Console.ReadLine()!.Split().Select(int.Parse).ToArray();//User input
+
+                for (int j = 0; j < col; j++)
+                {
+                    matrix[i, j] = rowElements[j];
+                }
+            }
+
+            int numberForSearch = int.Parse(Console.ReadLine()!);//User input
+            bool foundTheNumber = false;
+
+            foreach (int rowElement in matrix) // New way for searching with (foreach) loop 
+            {
+                if (rowElement == numberForSearch)
+                {
+                    foundTheNumber = true;
+                    break;
+                }
+            }
+
+            //for (int i = 0; i < row; i++) // Simble way with nested loop searching
+            //{
+            //    for (int j = 0; j < col; j++)
+            //    {
+            //        if (matrix[i, j] == numberForSearch)
+            //        {
+            //            foundTheNumber = true;
+            //            break;
+            //        }
+            //    }
+            //}
+
+            if (foundTheNumber)
+            {
+                Console.WriteLine("will not take number");
+            }
+            else
+            {
+                Console.WriteLine("will take number");
+            }
+        }
+    }
+}
