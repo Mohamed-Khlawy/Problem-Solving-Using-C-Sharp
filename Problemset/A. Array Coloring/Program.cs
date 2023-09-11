e3997de7663f0cdce418f2a54bbe59302452de1b
Khlawy <mhmdalkhlawy45@gmail.com>
2023-09-11 21:56:59 +0300

    Solved from Problemset : A. Park Lighting \\ A. Array Coloring

---
+++ Problemset/A. Array Coloring/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace A._Array_Coloring
+{
+    internal class Program
+    {
+
+        //problem Link: https://codeforces.com/problemset/problem/1857/A
+        static void Main(string[] args)
+        {
+            int tests = int.Parse(Console.ReadLine()!);
+            while (tests-- > 0)
+            {
+                int size = int.Parse(Console.ReadLine()!);
+                int[] numbersArray = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+
+                int counterForOddNumbers = 0;
+
+                for (int i = 0; i < numbersArray.Length; i++)
+                {
+                    if (numbersArray[i] % 2 != 0)
+                    {
+                        counterForOddNumbers++;
+                    }
+                }
+
+                if (counterForOddNumbers % 2 != 0)
+                {
+                    Console.WriteLine("NO");
+                }
+                else
+                {
+                    Console.WriteLine("YES");
+                }
+            }
+        }
+    }
+}
---
+++ Problemset/A. Park Lighting/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace A._Park_Lighting
+{
+    internal class Program
+    {
+        //Problem Link: https://codeforces.com/problemset/problem/1358/A
+        static void Main(string[] args)
+        {
+            int tests = int.Parse(Console.ReadLine()!);
+            while (tests-- > 0)
+            {
+                int[] rows_cols = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+                double rows = rows_cols[0];
+                double cols = rows_cols[1];
+
+                double minLanterns = 0;
+
+                if (cols % 2 == 0) //Even number of cols
+                {
+                    minLanterns = cols / 2 * rows;
+                }
+
+                else //Odd number of cols
+                {
+                    minLanterns = ((cols - 1) / 2 * rows) + (Math.Ceiling(rows / 2));
+                }
+
+                Console.WriteLine(minLanterns);
+            }
+        }
+    }
+}
