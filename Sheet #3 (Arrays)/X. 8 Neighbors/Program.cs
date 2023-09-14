d047a9f1309d8b58aaaf94a4566c122e99671c20
Khlawy <mhmdalkhlawy45@gmail.com>
2023-09-14 22:13:44 +0300

    Solved from Sheet #3 : X. 8 Neighbors \\ Y. Range sum query

---
+++ Sheet #3 (Arrays)/X. 8 Neighbors/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace X._8_Neighbors
+{
+    internal class Program
+    {
+        //This proplem was hard and i used Chat Gpt
+        static bool IsValid(int x, int y, int n, int m)
+        {
+            return x >= 0 && x < n && y >= 0 && y < m;
+        }
+
+        static bool AreAllNeighborsX(char[,] grid, int x, int y, int n, int m)
+        {
+            int[] dx = { -1, 1, 0, 0, -1, -1, 1, 1 };
+            int[] dy = { 0, 0, -1, 1, -1, 1, -1, 1 };
+
+            for (int i = 0; i < 8; i++)
+            {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+
+                if (IsValid(nx, ny, n, m) && grid[nx, ny] != 'x')
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+            int n = input[0];
+            int m = input[1];
+
+            char[,] grid = new char[n, m];
+
+            for (int i = 0; i < n; i++)
+            {
+                string row = Console.ReadLine()!;
+                for (int j = 0; j < m; j++)
+                {
+                    grid[i, j] = row[j];
+                }
+            }
+
+            int[] xy = Console.ReadLine()!.Split().Select(int.Parse)!.ToArray();
+            int x = xy[0] - 1; // Adjust for 0-based indexing
+            int y = xy[1] - 1; // Adjust for 0-based indexing
+
+            if (AreAllNeighborsX(grid, x, y, n, m))
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
---
+++ Sheet #3 (Arrays)/Y. Range sum query/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Y._Range_sum_query
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arraySize_numberOfQueries = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+
+            int arraySize = arraySize_numberOfQueries[0];
+            int numberOfQueries = arraySize_numberOfQueries[1];
+
+            long[] numbersArray = Console.ReadLine()!.Split().Select(long.Parse).ToArray();
+            long[] prefixSumArray = new long[arraySize];
+
+            prefixSumArray[0] = numbersArray[0];
+
+            for (int i = 1; i < arraySize; i++)
+            {
+                prefixSumArray[i] = numbersArray[i] + prefixSumArray[i - 1];
+            }
+
+            while (numberOfQueries-- > 0)
+            {
+                int[] left_right = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+
+                int left = left_right[0] - 1;
+                int right = left_right[1] - 1;
+
+                long rangeSum = prefixSumArray[right] - (left > 0 ? prefixSumArray[left - 1] : 0);
+
+                Console.WriteLine(rangeSum);
+            }
+        }
+    }
+}
