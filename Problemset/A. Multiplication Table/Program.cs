0bfb5b63810d56c7e9f8b0cc4338912d601f4ad6
Khlawy <mhmdalkhlawy45@gmail.com>
2023-09-16 21:54:13 +0300

    Solved from Proplemset : A. Multiplication Table \\ B. Construct the String

---
+++ Problemset/A. Multiplication Table/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace A._Multiplication_Table
+{
+    internal class Program
+    {
+        // Proplem Link: https://codeforces.com/problemset/problem/577/A
+        static void Main(string[] args)
+        {
+            double[] arraySize_numberForSearch = Console.ReadLine()!.Split().Select(double.Parse).ToArray();
+            double arraySize = arraySize_numberForSearch[0];
+            double numberForSearch = arraySize_numberForSearch[1];
+
+            int counterFornumberOccurences = 0;
+
+            for (int i = 1; i <= arraySize; i++)
+            {
+                if (numberForSearch % i == 0)
+                {
+                    if (numberForSearch / i <= arraySize)
+                    {
+                        counterFornumberOccurences += 1;
+                    }
+                }
+            }
+
+            Console.WriteLine(counterFornumberOccurences);
+        }
+    }
+}
---
+++ Problemset/B. Construct the String/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace B._Construct_the_String
+{
+    internal class Program
+    {
+        //Proplem Link: https://codeforces.com/problemset/problem/1335/B
+        static void Main(string[] args)
+        {
+            string letters = "abcdefghijklmnopqrstuvwxyz";
+            int tests = int.Parse(Console.ReadLine()!);
+            while (tests-- > 0)
+            {
+                int[] n_a_b = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+                int n = n_a_b[0];
+                int a = n_a_b[1];
+                int b = n_a_b[2];
+
+                string requiredLetters = letters.Substring(0, b);
+                string result = "";
+
+                for (int i = 0; i < n; i++)
+                {
+                    result += requiredLetters[i % b];
+                }
+
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
