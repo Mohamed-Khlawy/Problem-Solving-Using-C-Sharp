b6bb699de43095382cfbe17b22922ebbcebe3b7f
Khlawy <mhmdalkhlawy45@gmail.com>
2023-09-18 21:25:24 +0300

    Solved from Proplemset : B. Triple \\ B. Lecture

---
+++ Problemset/B. Lecture/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace B._Lecture
+{
+    internal class Program
+    {
+        //Proplem Link: https://codeforces.com/problemset/problem/499/BB
+        static void Main(string[] args)
+        {
+            int[] lectureWords_languageWords = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+            int numberOfLectureWords = lectureWords_languageWords[0];
+            int languageWords = lectureWords_languageWords[1];
+
+            string[] firstLanguage = new string[languageWords];
+            string[] secondLanguage = new string[languageWords];
+
+            for (int i = 0; i < languageWords; i++)
+            {
+                string[] equalWords = Console.ReadLine()!.Split();
+
+                firstLanguage[i] = equalWords[0];
+                secondLanguage[i] = equalWords[1];
+            }
+
+            string[] lectureWords = Console.ReadLine()!.Split();
+            string[] lectureSummary = new string[numberOfLectureWords];
+
+            for (int i = 0; i < numberOfLectureWords; i++)
+            {
+                int wordIndex = Array.IndexOf(firstLanguage, lectureWords[i]);
+
+                if (firstLanguage[wordIndex].Length > secondLanguage[wordIndex].Length)
+                {
+                    lectureSummary[i] = secondLanguage[wordIndex];
+                }
+
+                else
+                {
+                    lectureSummary[i] = firstLanguage[wordIndex];
+                }
+            }
+
+            for (int i = 0; i < numberOfLectureWords; i++)
+            {
+                Console.Write(lectureSummary[i] + " ");
+            }
+        }
+    }
+}
---
+++ Problemset/B. Triple/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace B._Triple
+{
+    internal class Program
+    {
+        //Proplem Link: https://codeforces.com/problemset/problem/1669/B
+        static void Main(string[] args)
+        {
+            int tests = int.Parse(Console.ReadLine()!);
+            while (tests-- > 0)
+            {
+                int arraySize = int.Parse(Console.ReadLine()!);
+                int[] numbersArray = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+
+                Array.Sort(numbersArray);
+                bool foundTriple = false;
+
+                for (int i = 0; i < numbersArray.Length - 2; i++)
+                {
+                    if (numbersArray[i] == numbersArray[i + 1] && numbersArray[i] == numbersArray[i + 2])
+                    {
+                        Console.WriteLine(numbersArray[i]);
+                        foundTriple = true;
+                        break;
+                    }
+                }
+
+                if (!foundTriple)
+                {
+                    Console.WriteLine(-1);
+                }
+            }
+        }
+    }
+}
