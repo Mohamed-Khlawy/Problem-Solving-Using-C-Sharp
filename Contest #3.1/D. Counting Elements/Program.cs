c23a62238d9268f66b649357d134620bfb73e50b
Khlawy <mhmdalkhlawy45@gmail.com>
2023-09-19 23:52:56 +0300

    Solved from Contest #3.1 : D. Counting Elements

---
+++ Contest #3.1/D. Counting Elements/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace D._Counting_Elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int arraySize = int.Parse(Console.ReadLine()!);
+            int[] numbersArray = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+
+            long counter = 0;
+
+            for (int i = 0; i < numbersArray.Length; i++)
+            {
+                if (numbersArray.Contains(numbersArray[i] + 1))
+                {
+                    counter++;
+                }
+            }
+
+            Console.WriteLine(counter);
+        }
+    }
+}
