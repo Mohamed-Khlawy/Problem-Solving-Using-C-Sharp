31fd48e89ca35d309e9bf0089e01a64d365709fa
Khlawy <mhmdalkhlawy45@gmail.com>
2023-09-20 21:25:34 +0300

    Solved from Proplemser : A. Business trip \\ B. Ten Words of Wisdom

---
+++ Problemset/A. Business trip/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace A._Business_trip
+{
+    internal class Program
+    {
+        //Proplem Link: https://codeforces.com/problemset/problem/149/A
+        static void Main(string[] args)
+        {
+            int fewestCentimeters = int.Parse(Console.ReadLine()!);
+            int[] growthForEachMounth = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+
+            Array.Sort(growthForEachMounth);
+
+            int minNumberOfMounths = 0;
+            int index = growthForEachMounth.Length - 1;
+
+            while (fewestCentimeters > 0 && index >= 0)
+            {
+                fewestCentimeters -= growthForEachMounth[index];
+                index--;
+                minNumberOfMounths++;
+            }
+
+            if (fewestCentimeters <= 0)
+            {
+                Console.WriteLine(minNumberOfMounths);
+            }
+            else
+            {
+                Console.WriteLine(-1);
+            }
+        }
+    }
+}
---
+++ Problemset/B. Ten Words of Wisdom/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace B._Ten_Words_of_Wisdom
+{
+    internal class Program
+    {
+        //Proplem Link: https://codeforces.com/problemset/problem/1850/B
+        static void Main(string[] args)
+        {
+            int tests = int.Parse(Console.ReadLine()!);
+            while (tests-- > 0)
+            {
+                int numberOfResponses = int.Parse(Console.ReadLine()!);
+
+                int winnerResponse = 0;
+                int index = 0;
+                int higestQuality = 0;
+
+                while (numberOfResponses-- > 0)
+                {
+                    int[] numberOfWords_quality = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+
+                    if (numberOfWords_quality[0] <= 10)
+                    {
+                        if (winnerResponse == 0)
+                        {
+                            winnerResponse = index + 1;
+                            higestQuality = numberOfWords_quality[1];
+                        }
+                        else if (winnerResponse != 0 && higestQuality < numberOfWords_quality[1])
+                        {
+                            winnerResponse = index + 1;
+                            higestQuality = numberOfWords_quality[1];
+                        }
+                    }
+
+                    index++;
+                }
+
+                Console.WriteLine(winnerResponse);
+            }
+        }
+    }
+}
