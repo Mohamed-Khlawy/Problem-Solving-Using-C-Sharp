4be19c8963b7ee11769d0149b37aeb6383a289ed
Khlawy <mhmdalkhlawy45@gmail.com>
2023-09-23 21:52:31 +0300

    Solved form Problemset : B. Who's Opposite? \\ B. Blank Space

---
+++ Problemset/B. Blank Space/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace B._Blank_Space
+{
+    internal class Program
+    {
+        //Problem Link: https://codeforces.com/problemset/problem/1829/B
+        static void Main(string[] args)
+        {
+            int tests = int.Parse(Console.ReadLine()!);
+            while (tests-- > 0)
+            {
+                int size = int.Parse(Console.ReadLine()!);
+                int[] numbersArray = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+
+                List<int> lengthsOfZero = new List<int>();
+                int zeroCounter = 0;
+
+                for (int i = 0; i < numbersArray.Length; i++)
+                {
+                    if (numbersArray[i] == 0)
+                    {
+                        zeroCounter++;
+                    }
+                    else if (numbersArray[i] == 1)
+                    {
+                        lengthsOfZero.Add(zeroCounter);
+                        zeroCounter = 0;
+                    }
+                }
+
+                if (zeroCounter != 0)
+                {
+                    lengthsOfZero.Add(zeroCounter);
+                }
+
+                lengthsOfZero.Sort();
+
+                Console.WriteLine(lengthsOfZero[lengthsOfZero.Count - 1]);
+            }
+        }
+    }
+}
---
+++ Problemset/B. Who's Opposite/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace B._Who_s_Opposite
+{
+    internal class Program
+    {
+        //Problem Link: https://codeforces.com/problemset/problem/1560/B
+        static void Main(string[] args)
+        {
+            int tests = int.Parse(Console.ReadLine()!);
+            while (tests-- > 0)
+            {
+                double[] a_b_c = Console.ReadLine()!.Split().Select(double.Parse).ToArray();
+
+                double a = a_b_c[0];
+                double b = a_b_c[1];
+                double c = a_b_c[2];
+
+                double difference = Math.Abs(a - b);
+                double totalNumbers = difference * 2;
+
+                if (difference < 2 || a > totalNumbers || b > totalNumbers || c > totalNumbers
+                    || Math.Max(a, b) != Math.Min(a, b) + difference)
+                {
+                    Console.WriteLine(-1);
+                }
+
+                else
+                {
+                    if (c <= difference)
+                    {
+                        Console.WriteLine(c + difference);
+                    }
+                    else
+                    {
+                        Console.WriteLine(c - difference);
+                    }
+                }
+            }
+        }
+    }
+}
