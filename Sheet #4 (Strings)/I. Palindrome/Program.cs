582c86ec8d390f47662e2c47474a71b9ec48242a
Khlawy <mhmdalkhlawy45@gmail.com>
2023-09-29 00:06:00 +0300

    Solved form Sheet #4 : I. Palindrome

---
+++ Sheet #4 (Strings)/I. Palindrome/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace I._Palindrome
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine()!;
+
+            char[] wordChars = word.ToCharArray();
+
+            Array.Reverse(wordChars);
+
+            string reversedWord = new string(wordChars);
+
+            if (word == reversedWord)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
