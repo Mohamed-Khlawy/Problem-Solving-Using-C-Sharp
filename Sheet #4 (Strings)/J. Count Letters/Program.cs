c6fe8adcbcba6fb13c0480264aa357a4959aea97
Khlawy <mhmdalkhlawy45@gmail.com>
2023-09-29 19:54:05 +0300

    Solved from Sheet #4 : J. Count Letters

---
+++ Sheet #4 (Strings)/J. Count Letters/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace J._Count_Letters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine()!;
+
+            string alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+            Dictionary<char, int> lettersCounter = new Dictionary<char, int>();
+
+            for (int i = 0; i < alphabet.Length; i++)
+            {
+                lettersCounter.Add(alphabet[i], 0);
+            }
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                lettersCounter[word[i]]++;
+            }
+
+            foreach (var letter in lettersCounter)
+            {
+                if (letter.Value > 0)
+                {
+                    Console.WriteLine($"{letter.Key} : {letter.Value}");
+                }
+            }
+        }
+    }
+}
