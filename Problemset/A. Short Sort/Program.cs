1df521c4284cd06f9dea84a70560e58ed8e5b3e1
Khlawy <mhmdalkhlawy45@gmail.com>
2023-09-30 22:02:49 +0300

    Solved from Problemset : A. Short Sort \\ B. Good Kid

---
+++ Problemset/A. Short Sort/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace A._Short_Sort
+{
+    internal class Program
+    {
+        //Problem Link: https://codeforces.com/problemset/problem/1873/A
+        static void Main(string[] args)
+        {
+            int tests = int.Parse(Console.ReadLine()!);
+            while (tests-- > 0)
+            {
+                string abcOrder = Console.ReadLine()!;
+
+                if (abcOrder[0] != 'a' && abcOrder[1] != 'b' && abcOrder[2] != 'c')
+                {
+                    Console.WriteLine("NO");
+                }
+                else
+                {
+                    Console.WriteLine("YES");
+                }
+            }
+        }
+    }
+}
---
+++ Problemset/B. Good Kid/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace B._Good_Kid
+{
+    internal class Program
+    {
+        //Problem Link: https://codeforces.com/problemset/problem/1873/B
+        static void Main(string[] args)
+        {
+            int tests = int.Parse(Console.ReadLine()!);
+            while (tests-- > 0)
+            {
+                int size = int.Parse(Console.ReadLine()!);
+                int[] numbersArray = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+
+                Array.Sort(numbersArray);
+
+                numbersArray[0]++;
+
+                long summation = 1;
+
+                for (int i = 0; i < numbersArray.Length; i++)
+                {
+                    summation *= numbersArray[i];
+                }
+
+                Console.WriteLine(summation);
+            }
+        }
+    }
+}
