df9fc96ac4ba6ace76cf70b58c9cce72441ae8b3
Khlawy <mhmdalkhlawy45@gmail.com>
2023-10-02 23:56:08 +0300

    Solved from Problemset : B. Kana and Dragon Quest game

---
+++ Problemset/B. Kana and Dragon Quest game/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace B._Kana_and_Dragon_Quest_game
+{
+    internal class Program
+    {
+        //Problem Link: https://codeforces.com/problemset/problem/1337/B
+        static void Main(string[] args)
+        {
+            int tests = int.Parse(Console.ReadLine()!);
+            while (tests-- > 0)
+            {
+                int[] h_n_m = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+
+                int h = h_n_m[0];
+                int copy_h = h;
+                int n = h_n_m[1];
+                int m = h_n_m[2];
+
+                while (h > 0 && n-- > 0)
+                {
+                    h = (h / 2) + 10;
+                }
+
+                if (h <= 0)
+                {
+                    Console.WriteLine("YES");
+                }
+
+                else if (copy_h < h)
+                {
+                    copy_h -= (10 * m);
+
+                    if (copy_h <= 0)
+                    {
+                        Console.WriteLine("YES");
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("NO");
+                    }
+                }
+
+                else
+                {
+                    h -= (10 * m);
+
+                    if (h <= 0)
+                    {
+                        Console.WriteLine("YES");
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("NO");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Sheet #4 (Strings)/K. I Love strings/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace K._I_Love_strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int tests = int.Parse(Console.ReadLine()!);
+            while (tests-- > 0)
+            {
+                string[] input = Console.ReadLine()!.Split();
+                string firstWord = input[0];
+                string secondWord = input[1];
+
+                int maxWordSize = Math.Max(firstWord.Length, secondWord.Length);
+                string resultWord = "";
+
+                for (int i = 0; i < maxWordSize; i++)
+                {
+                    if (i < firstWord.Length)
+                    {
+                        resultWord += firstWord[i];
+                    }
+                    if (i < secondWord.Length)
+                    {
+                        resultWord += secondWord[i];
+                    }
+                }
+
+                Console.WriteLine(resultWord);
+            }
+        }
+    }
+}
