de2dda7963f35cb08f4007471209fbe273672c16
Khlawy <mhmdalkhlawy45@gmail.com>
2023-10-06 22:00:04 +0300

    Solved from Sheet #4 : O. Sort String \\ P. Count Words

---
+++ Sheet #4 (Strings)/O. Sort String/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace O._Sort_String
+{
+    internal class Program
+    {
+        //This problem got Memory Limit Exceded with this code but got 
+        //accepted with same code in C++
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine()!);
+            string word = Console.ReadLine()!;
+
+            int[] charsFrequency = new int[26];
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                charsFrequency[(int)word[i] - 97]++;
+            }
+
+            for (int i = 0; i < charsFrequency.Length; i++)
+            {
+                while (charsFrequency[i] != 0)
+                {
+                    Console.Write((char)(i + 97));
+                    charsFrequency[i]--;
+                }
+            }
+        }
+    }
+}
---
+++ Sheet #4 (Strings)/P. Count Words/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace P._Count_Words
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] seperators = { ' ', '!', '?', ',', '.' };
+            string[] wordsOfSentence = Console.ReadLine()!.Split(seperators);
+
+            long wordsCounter = 0;
+
+            for (int i = 0; i < wordsOfSentence.Length; i++)
+            {
+                if (wordsOfSentence[i] != "" && char.IsLetter(wordsOfSentence[i][0]))
+                {
+                    wordsCounter++;
+                }
+            }
+
+            Console.WriteLine(wordsCounter);
+        }
+    }
+}
