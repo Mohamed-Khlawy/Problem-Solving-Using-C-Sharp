166041e2cc903767f7b4bf3c52f9d5a83ca37a4b
Khlawy <mhmdalkhlawy45@gmail.com>
2023-10-07 21:53:53 +0300

    Solved from Problemset : A. Two Vessels \\ A. System of Equations

---
+++ Problemset/A. System of Equations/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace A._System_of_Equations
+{
+    internal class Program
+    {
+        //I saw it's Tutorial
+        //Problem Link: https://codeforces.com/problemset/problem/214/A
+        static void Main(string[] args)
+        {
+            int[] n_m = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+
+            int n = n_m[0];
+            int m = n_m[1];
+
+            long numberOfPairs = 0;
+
+            for (int a = 0; a * a <= n && a <= m; a++)
+            {
+                int b = n - a * a;
+
+                if (a + b * b == m)
+                {
+                    numberOfPairs++;
+                }
+            }
+
+            Console.WriteLine(numberOfPairs);
+        }
+    }
+}
---
+++ Problemset/A. Two Vessels/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace A._Two_Vessels
+{
+    internal class Program
+    {
+        //Problem Link: https://codeforces.com/problemset/problem/1872/A
+        static void Main(string[] args)
+        {
+            int tests = int.Parse(Console.ReadLine()!);
+            while (tests-- > 0)
+            {
+                int[] a_b_c = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+
+                int a = a_b_c[0];
+                int b = a_b_c[1];
+                int c = a_b_c[2];
+
+                int maxA_B = Math.Max(a, b);
+                int minA_B = Math.Min(a, b);
+
+                long numberOfMoves = 0;
+
+                while (maxA_B > minA_B)
+                {
+                    maxA_B -= c;
+                    minA_B += c;
+                    numberOfMoves++;
+                }
+
+                Console.WriteLine("*" + numberOfMoves);
+            }
+        }
+    }
+}
