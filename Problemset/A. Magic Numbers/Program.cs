e13234babcd15b370418a1c8377d66a93ef8798c
Khlawy <mhmdalkhlawy45@gmail.com>
2023-10-08 21:43:47 +0300

    Solved from Problemset : A. Magic Numbers \\ A. QAQ

---
+++ Problemset/A. Magic Numbers/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace A._Magic_Numbers
+{
+    internal class Program
+    {
+        //Problem Link: https://codeforces.com/problemset/problem/320/A
+        static void Main(string[] args)
+        {
+            string number = Console.ReadLine()!;
+
+            if (number[0] != '1' || number.Contains("444"))
+            {
+                Console.WriteLine("NO");
+                return;
+            }
+            else
+            {
+                bool notOneNorFour = false;
+
+                for (int i = 1; i < number.Length; i++)
+                {
+                    if (number[i] != '1' && number[i] != '4')
+                    {
+                        notOneNorFour = true;
+                        break;
+                    }
+                }
+
+                if (!notOneNorFour)
+                {
+                    Console.WriteLine("YES");
+                }
+                else
+                {
+                    Console.WriteLine("NO");
+                }
+            }
+        }
+    }
+}
---
+++ Problemset/A. QAQ/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace A._QAQ
+{
+    internal class Program
+    {
+        //Problem Link: https://codeforces.com/problemset/problem/894/A
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine()!;
+
+            long numberOfQAQ = 0;
+
+            for (int i = 0; i < word.Length; i++)
+            {
+
+                if (word[i] == 'Q')
+                {
+
+                    for (int j = i + 1; j < word.Length; j++)
+                    {
+
+                        if (word[j] == 'A')
+                        {
+
+                            for (int k = j + 1; k < word.Length; k++)
+                            {
+
+                                if (word[k] == 'Q')
+                                {
+
+                                    numberOfQAQ++;
+
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine(numberOfQAQ);
+        }
+    }
+}
