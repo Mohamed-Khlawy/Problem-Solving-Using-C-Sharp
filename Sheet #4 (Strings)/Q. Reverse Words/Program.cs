689e4190af4c7d6a4ddcdacca4142ddfa496ea6c
Khlawy <mhmdalkhlawy45@gmail.com>
2023-10-10 22:36:51 +0300

    Solved from Sheet #4 : Q. Reverse Words

---
+++ Sheet #4 (Strings)/Q. Reverse Words/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Q._Reverse_Words
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] sentence = Console.ReadLine()!.Split();
+
+            for (int i = 0; i < sentence.Length; i++)
+            {
+                char[] chars = sentence[i].ToCharArray();
+
+                Array.Reverse(chars);
+
+                sentence[i] = new string(chars);
+
+                if (i != sentence.Length - 1)
+                {
+                    Console.Write(sentence[i] + " ");
+                }
+
+                else
+                {
+                    Console.Write(sentence[i]);
+                }
+            }
+        }
+    }
+}
