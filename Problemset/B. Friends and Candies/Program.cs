f0ac1186ff08befa6958e60db05273e66ca4203f
Khlawy <mhmdalkhlawy45@gmail.com>
2023-10-16 21:49:55 +0300

    Solved from Problemset : B. Friends and Candies

---
+++ Problemset/B. Friends and Candies/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace B._Friends_and_Candies
+{
+    internal class Program
+    {
+        //Problem Link: https://codeforces.com/problemset/problem/1538/B
+        static void Main(string[] args)
+        {
+            int tests = int.Parse(Console.ReadLine()!);
+            while (tests-- > 0)
+            {
+                int size = int.Parse(Console.ReadLine()!);
+                int[] candiesArray = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+
+                long totalCandies = candiesArray.Sum();
+
+                if (totalCandies % size != 0)
+                {
+                    Console.WriteLine(-1);
+                }
+
+                else
+                {
+                    long maxNumberOfCandies = totalCandies / size;
+
+                    int minNumberOfFriends = 0;
+
+                    foreach (var cand in candiesArray)
+                    {
+                        if (cand > maxNumberOfCandies)
+                        {
+                            minNumberOfFriends++;
+                        }
+                    }
+
+                    Console.WriteLine(minNumberOfFriends);
+                }
+            }
+        }
+    }
+}
