93ea7ab12c43ff2c982e5f2e4047dd55d9ce19a8
Khlawy <mhmdalkhlawy45@gmail.com>
2023-10-18 22:32:54 +0300

    Solved from Problemset : C. Odd/Even Increments

---
+++ Problemset/C. Odd_Even Increments/Program.cs
@@ -0,0 +1,77 @@
+﻿namespace C._Odd_Even_Increments
+{
+    internal class Program
+    {
+        //Problem Link: https://codeforces.com/problemset/problem/1669/C
+        static void Main(string[] args)
+        {
+            int tests = int.Parse(Console.ReadLine()!);
+            while (tests-- > 0)
+            {
+                int size = int.Parse(Console.ReadLine()!);
+
+                int[] numbersAraay = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+
+                bool isEven = false;
+                bool isOdd = false;
+                bool weCan = true;
+
+                if (numbersAraay[0] % 2 == 0)
+                {
+                    isEven = true;
+                }
+                else
+                {
+                    isOdd = true;
+                }
+
+                for (int i = 2; i < numbersAraay.Length; i += 2)
+                {
+                    if ((numbersAraay[i] % 2 == 0 && !isEven) ||
+                        (numbersAraay[i] % 2 != 0 && !isOdd))
+                    {
+                        weCan = false;
+                    }
+                }
+
+                if (weCan)
+                {
+                    isEven = false;
+                    isOdd = false;
+
+                    if (numbersAraay[1] % 2 == 0)
+                    {
+                        isEven = true;
+                    }
+                    else
+                    {
+                        isOdd = true;
+                    }
+
+                    for (int i = 3; i < numbersAraay.Length; i += 2)
+                    {
+                        if ((numbersAraay[i] % 2 == 0 && !isEven) ||
+                            (numbersAraay[i] % 2 != 0 && !isOdd))
+                        {
+                            weCan = false;
+                        }
+                    }
+
+                    if (weCan)
+                    {
+                        Console.WriteLine("YES");
+                    }
+                    else
+                    {
+                        Console.WriteLine("NO");
+                    }
+                }
+
+                else
+                {
+                    Console.WriteLine("NO");
+                }
+            }
+        }
+    }
+}
