ab1feca13a7dc64318810ce987db715a52219d5d
Khlawy <mhmdalkhlawy45@gmail.com>
2023-10-21 00:11:01 +0300

    Solved from Sheet #4 : T. URL

---
+++ Sheet #4 (Strings)/T. URL/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace T._URL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //http://www.cleartrip.com/signin/service?username=test&pwd=test&profile=developer&role=ELITE&key=manager
+            string url = Console.ReadLine()!;
+
+            url = url.Substring(url.IndexOf('?') + 1);
+
+            for (int i = 0; i < url.Length; i++)
+            {
+                if (url[i] == '=')
+                {
+                    Console.Write(": ");
+                    continue;
+                }
+                if (url[i] == '&')
+                {
+                    Console.WriteLine();
+                    continue;
+                }
+
+                Console.Write(url[i]);
+            }
+        }
+    }
+}
