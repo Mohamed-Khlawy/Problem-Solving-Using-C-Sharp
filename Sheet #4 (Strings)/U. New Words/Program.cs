bba6deb34caa65a5fde0c621ac447eb32a2151fe
Khlawy <mhmdalkhlawy45@gmail.com>
2023-10-21 23:18:29 +0300

    Solved from Sheet #4 : U. New Words

---
+++ Sheet #4 (Strings)/U. New Words/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace U._New_Words
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine()!;
+
+            word = word.ToLower();
+
+            Dictionary<char, int> egyptChars = new Dictionary<char, int>();
+
+            string egypt = "egypt";
+
+            for (int i = 0; i < egypt.Length; i++)
+            {
+                egyptChars.Add(egypt[i], 0);
+            }
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (egypt.Contains(word[i]))
+                {
+                    egyptChars[word[i]]++;
+                }
+            }
+
+            int minValue = egyptChars['e'];
+
+            foreach (var kvp in egyptChars)
+            {
+                if (kvp.Value < minValue)
+                {
+                    minValue = kvp.Value;
+                }
+            }
+
+            Console.WriteLine(minValue);
+        }
+    }
+}
