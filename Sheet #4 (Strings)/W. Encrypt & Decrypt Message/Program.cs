03ddc05d1f21bee748b9225d2c8422ee1c5d1f5a
Khlawy <mhmdalkhlawy45@gmail.com>
2023-10-25 23:44:52 +0300

    Solved from Sheet #4 : W. Encrypt & Decrypt Message

---
+++ Sheet #4 (Strings)/W. Encrypt & Decrypt Message/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace W._Encrypt___Decrypt_Message
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine()!;
+
+            word = word.Replace("EGYPT", " ");
+
+            Console.WriteLine(word);
+        }
+    }
+}
