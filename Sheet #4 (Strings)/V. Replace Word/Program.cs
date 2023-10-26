96728bd3991e3df58788e27bc32da6b4a84ed3e1
Khlawy <mhmdalkhlawy45@gmail.com>
2023-10-26 23:37:36 +0200

    Solved from Sheet #4 : V. Replace Word \\ W. Encrypt & Decrypt Message

---
+++ Sheet #4 (Strings)/V. Replace Word/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace V._Replace_Word
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine()!;
+
+            word = word.Replace("EGYPT", " ");
+
+            Console.WriteLine(word);
+        }
+    }
+}
--- Sheet #4 (Strings)/W. Encrypt & Decrypt Message/Program.cs
+++ Sheet #4 (Strings)/W. Encrypt & Decrypt Message/Program.cs
@@ -4,11 +4,37 @@
     {
         static void Main(string[] args)
         {
+            string key = "PgEfTYaWGHjDAmxQqFLRpCJBownyUKZXkbvzIdshurMilNSVOtec#@_!=.+-*/";
+            string original = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+            int number = int.Parse(Console.ReadLine()!);
             string word = Console.ReadLine()!;
 
-            word = word.Replace("EGYPT", " ");
+            if (number == 1)
+            {
+                string result = "";
 
-            Console.WriteLine(word);
+                for (int i = 0; i < word.Length; i++)
+                {
+                    int index = original.IndexOf(word[i]);
+                    result += key[index];
+                }
+
+                Console.WriteLine(result);
+            }
+
+            else
+            {
+                string result = "";
+
+                for (int i = 0; i < word.Length; i++)
+                {
+                    int index = key.IndexOf(word[i]);
+                    result += original[index];
+                }
+
+                Console.WriteLine(result);
+            }
         }
     }
 }
