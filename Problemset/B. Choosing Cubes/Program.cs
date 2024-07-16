7a56339f4b7a4905935ca05190ce73daffaf14b0
Khlawy <mhmdalkhlawy45@gmail.com>
2024-07-16 18:45:07 +0300

    Solved from Probelmset: B. Choosing Cubes

---
+++ Problemset/B. Choosing Cubes/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace B._Choosing_Cubes
+{
+    internal class Program
+    {
+        // Problem Link : https://codeforces.com/problemset/problem/1980/B
+        static void Main(string[] args)
+        {
+            int tests = int.Parse(Console.ReadLine()!);
+
+            while (tests-- > 0)
+            {
+                int[] n_f_k = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+
+                int n = n_f_k[0];
+                int f = n_f_k[1];
+                int k = n_f_k[2];
+
+                List<int> cubes = Console.ReadLine()!.Split().Select(int.Parse).ToList();
+
+                int f_value = cubes[f - 1];
+
+                cubes.Sort();
+
+                cubes.Reverse();
+
+                int total_f_in_cubes = cubes.Where(x => x == f_value).Count();
+
+                int total_f_in_sub_cubes = 0;
+
+                for (int i = 0; i < k; i++)
+                {
+                    if (cubes[i] == f_value)
+                    {
+                        total_f_in_sub_cubes++;
+                    }
+                }
+
+                //Console.Write("***");
+
+                if (total_f_in_sub_cubes < total_f_in_cubes && total_f_in_sub_cubes != 0)
+                {
+                    Console.WriteLine("MAYBE");
+                }
+
+                else if (total_f_in_sub_cubes == total_f_in_cubes)
+                {
+                    Console.WriteLine("YES");
+                }
+
+                else if (total_f_in_sub_cubes == 0)
+                {
+                    Console.WriteLine("NO");
+                }
+            }
+        }
+    }
+}
--- Problemset/B. Construct the String/obj/Debug/net6.0/B. Construct the String.AssemblyInfo.cs
+++ Problemset/B. Construct the String/obj/Debug/net6.0/B. Construct the String.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("B. Construct the String")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d9ab0a279d2484baebb0b42342b68b06a016894")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+708f129ae70466aaa57063d21d78f0c81e44f98c")]
 [assembly: System.Reflection.AssemblyProductAttribute("B. Construct the String")]
 [assembly: System.Reflection.AssemblyTitleAttribute("B. Construct the String")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
