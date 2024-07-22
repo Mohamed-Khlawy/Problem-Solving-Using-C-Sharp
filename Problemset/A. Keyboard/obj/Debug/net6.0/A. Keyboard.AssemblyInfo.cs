193b081a33243079b317a270edfa21ba7f62cb8e
Khlawy <mhmdalkhlawy45@gmail.com>
2024-07-22 18:30:32 +0300

    Solved from Problemset: A. Maximize?

--- Problemset/A. Keyboard/obj/Debug/net6.0/A. Keyboard.AssemblyInfo.cs
+++ Problemset/A. Keyboard/obj/Debug/net6.0/A. Keyboard.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("A. Keyboard")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f8f0b1d4fe43c95f4b996b37671e93de690f4ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83702cb195e7c3d8d98e65f72c4238df7ee8b9ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("A. Keyboard")]
 [assembly: System.Reflection.AssemblyTitleAttribute("A. Keyboard")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Problemset/A. Maximize/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace A._Maximize
+{
+    internal class Program
+    {
+        // Problem Link : https://codeforces.com/problemset/problem/1968/A
+        static void Main(string[] args)
+        {
+            int tests = int.Parse(Console.ReadLine()!);
+
+            while (tests-- > 0)
+            {
+                int a = int.Parse(Console.ReadLine()!);
+
+                Console.WriteLine(a - 1);
+            }
+        }
+    }
+}
