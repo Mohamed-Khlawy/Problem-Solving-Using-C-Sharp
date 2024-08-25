941505c8b1174676b0f7f5a010d71dfe4f50090d
Khlawy <mhmdalkhlawy45@gmail.com>
2024-08-25 23:35:19 +0300

    Solved from Problemset: A. Legs /// A. Recovering a Small String

---
+++ Problemset/A. Legs/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace A._Legs
+{
+    internal class Program
+    {
+        // Problem Link : https://codeforces.com/problemset/problem/1996/A
+        static void Main(string[] args)
+        {
+            int tests = int.Parse(Console.ReadLine()!);
+
+            while (tests-- > 0)
+            {
+                int number_of_legs = int.Parse(Console.ReadLine()!);
+
+                if (number_of_legs == 2)
+                {
+                    Console.WriteLine(1);
+                }
+
+                else if (number_of_legs % 4 == 0)
+                {
+                    Console.WriteLine(number_of_legs / 4);
+                }
+
+                else if (number_of_legs % 4 == 2)
+                {
+                    Console.WriteLine((number_of_legs - 2) / 4 + 1);
+                }
+            }
+        }
+    }
+}
---
+++ Problemset/A. Recovering a Small String/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace A._Recovering_a_Small_String
+{
+    internal class Program
+    {
+        // Problem Link : https://codeforces.com/problemset/problem/1931/A
+        static void Main(string[] args)
+        {
+            int tests = int.Parse(Console.ReadLine()!);
+
+            while (tests-- > 0)
+            {
+                int encoded_word = int.Parse(Console.ReadLine()!);
+
+                string alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+                char[] last_word = { 'a', 'a', 'a' };
+
+                for (int i = 2; i >= 0; i--)
+                {
+                    int encoded_without_last = encoded_word - i;
+
+                    if (encoded_without_last <= 26)
+                    {
+                        last_word[i] = alphabet[encoded_without_last - 1];
+                        break;
+                    }
+
+                    else
+                    {
+                        last_word[i] = 'z';
+                        encoded_word -= 26;
+                    }
+                }
+
+                Console.WriteLine(last_word[0].ToString() +
+                                  last_word[1].ToString() +
+                                  last_word[2].ToString());
+            }
+        }
+    }
+}
