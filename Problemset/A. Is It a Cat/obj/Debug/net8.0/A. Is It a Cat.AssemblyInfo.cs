b648845b32f70c66f71522cccef35fd139f86705
Khlawy <mhmdalkhlawy45@gmail.com>
2024-09-15 18:33:02 +0300

    Solved from Problemset: B. Present from Lena

--- Problemset/A. Is It a Cat/obj/Debug/net8.0/A. Is It a Cat.AssemblyInfo.cs
+++ Problemset/A. Is It a Cat/obj/Debug/net8.0/A. Is It a Cat.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("A. Is It a Cat")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0327434378eab970775742e9f3326635aa1f163")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+703629560611d2db05c92f9dfa36b237676aab8a")]
 [assembly: System.Reflection.AssemblyProductAttribute("A. Is It a Cat")]
 [assembly: System.Reflection.AssemblyTitleAttribute("A. Is It a Cat")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Problemset/B. Present from Lena/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace B._Present_from_Lena
+{
+    internal class Program
+    {
+        // Problem Link : https://codeforces.com/problemset/problem/118/B
+
+        static void PrintLeadingZeros(int n, int i)
+        {
+            for (int j = 0; j < (n - i) * 2; j++)
+            {
+                Console.Write(" ");
+            }
+        }
+
+        static void PrintRowNumbers(int i)
+        {
+            // Print Incresing Numbers including middle element for each row
+            for (int j = 0; j <= i; j++)
+            {
+                if (j == i)
+                {
+                    Console.Write(j);
+                }
+
+                else
+                {
+                    Console.Write(j + " ");
+                }
+            }
+
+            // Print Decreasing Numbers without middle element for each row
+            for (int j = i - 1; j >= 0; j--)
+            {
+                if (j == 0)
+                {
+                    Console.Write(" " + j);
+                }
+
+                else
+                {
+                    Console.Write(" " + j);
+                }
+            }
+
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine()!);
+
+            // Upper Part of rhombus (including the middle row)
+            for (int i = 0; i <= number; i++)
+            {
+                PrintLeadingZeros(number, i);
+
+                PrintRowNumbers(i);
+            }
+
+            // Lower Part of rhombus (excluding the middle row)
+            for (int i = number - 1; i >= 0; i--)
+            {
+                PrintLeadingZeros(number, i);
+
+                PrintRowNumbers(i);
+            }
+        }
+    }
+}
