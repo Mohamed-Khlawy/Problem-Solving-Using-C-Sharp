cb040dedc7d82965c70afa19fd594e3602a5d980
Khlawy <mhmdalkhlawy45@gmail.com>
2025-01-06 20:45:38 +0200

    Solved from Problemset: B. Swap and Delete

--- Problemset/A. Polycarp and Sums of Subsequences/obj/Debug/net8.0/A. Polycarp and Sums of Subsequences.AssemblyInfo.cs
+++ Problemset/A. Polycarp and Sums of Subsequences/obj/Debug/net8.0/A. Polycarp and Sums of Subsequences.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("A. Polycarp and Sums of Subsequences")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8eb9ef89a886468c0206f4daaa6e3f5c73af012d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f297a641520f0663ca4ea52c2f5c56b288872c65")]
 [assembly: System.Reflection.AssemblyProductAttribute("A. Polycarp and Sums of Subsequences")]
 [assembly: System.Reflection.AssemblyTitleAttribute("A. Polycarp and Sums of Subsequences")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Problemset/B. Swap and Delete/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace B._Swap_and_Delete
+{
+    internal class Program
+    {
+        // Problem Link : https://codeforces.com/problemset/problem/1913/B
+        static void Main(string[] args)
+        {
+            int tests = int.Parse(Console.ReadLine()!);
+
+            while (tests-- > 0)
+            {
+                string intial_str = Console.ReadLine()!;
+
+                //List<char> resulting_str = new List<char>();
+
+                int zero_counter = intial_str.Where(c => c == '0').Count();
+
+                int one_counter = intial_str.Where(c => c == '1').Count();
+
+                int resulting_str_size = 0;
+
+                //Console.Write("*********************************** ");
+
+                while (zero_counter > 0 || one_counter > 0)
+                {
+                    if (intial_str[resulting_str_size] == '1' && zero_counter > 0)
+                    {
+                        //resulting_str.Add('0');
+                        resulting_str_size++;
+                        zero_counter--;
+                    }
+
+                    else if (intial_str[resulting_str_size] == '0' && one_counter > 0)
+                    {
+                        //resulting_str.Add('1');
+                        resulting_str_size++;
+                        one_counter--;
+                    }
+
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                //Console.WriteLine(intial_str.Length - resulting_str.Count);
+
+                // For Space Complexity we won't make List of chars
+                // and solve it with counter of chars in resulting string
+                Console.WriteLine(intial_str.Length - resulting_str_size);
+            }
+        }
+    }
+}
