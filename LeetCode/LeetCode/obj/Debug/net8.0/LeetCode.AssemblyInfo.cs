ed79c017b5851b012668bdcf847585ca02e8f356
Khlawy <mhmdalkhlawy45@gmail.com>
2026-06-15 22:46:40 +0300

    Solved from LeetCode Problemset: Remove Duplicates from Sotred List.

--- LeetCode/LeetCode/obj/Debug/net8.0/LeetCode.AssemblyInfo.cs
+++ LeetCode/LeetCode/obj/Debug/net8.0/LeetCode.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LeetCode")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d272d372816e2c32eb6366b22a934742c757678")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+414be24b21feb513912cfe00522951f7c10cb4b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("LeetCode")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LeetCode")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ LeetCode/Remove Duplicates from Sotred List/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Remove_Duplicates_from_Sotred_List
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode? next;
+        public ListNode(int val = 0, ListNode? next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+    internal class Program
+    {
+        public ListNode? DeleteDuplicates(ListNode head)
+        {
+            if (head == null) return null; // Check if the list is empty
+
+            ListNode current = head; // Start from the head of the list
+
+            while (current != null && current.next != null)
+            {
+                if (current.val == current.next.val)
+                {
+                    current.next = current.next.next; // Skip the duplicate node
+                }
+                else
+                {
+                    current = current.next; // Move to the next node
+                }
+            }
+
+            return head; // Return the modified list
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+
+            // Example usage:
+            ListNode head = new ListNode(1, new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(3)))));
+
+            ListNode? result = p.DeleteDuplicates(head);
+
+            // Print the result
+            while (result != null)
+            {
+                Console.Write(result.val + " ");
+                result = result.next;
+            }
+        }
+    }
+}
