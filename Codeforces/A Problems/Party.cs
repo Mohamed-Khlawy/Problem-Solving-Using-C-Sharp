99b9059a76b8affdbb4a1618c24345d3acaddd4b
Khlawy <mhmdalkhlawy45@gmail.com>
2026-07-14 18:59:30 +0300

    Solved from Codeforces Problemset: A. Party.

---
+++ Codeforces/A Problems/Party.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A_Problems
+{
+    // Problem Link => https://codeforces.com/problemset/problem/115/A
+    internal class Party
+    {
+        public static int CalculateDepth(int employee, int[] managers, int[] depths)
+        {
+            if (depths[employee] != 0) return depths[employee];
+
+            if (managers[employee] == -1)
+            {
+                depths[employee] = 1;
+                return depths[employee];
+            }
+
+            depths[employee] = CalculateDepth(managers[employee] - 1, managers, depths) + 1;
+
+            return depths[employee];
+        }
+        public static int CalculateMinGroupsOrMaxDepth(int[] managers)
+        {
+            int[] depths = new int[managers.Length];
+
+            int maxDepth = 0;
+
+            for (int empIndex = 0; empIndex < managers.Length; empIndex++)
+            {
+                int tempDepth = CalculateDepth(empIndex, managers, depths);
+                maxDepth = Math.Max(maxDepth, tempDepth);
+            }
+
+            return maxDepth;
+        }
+    }
+}
--- Codeforces/A. Candies/obj/Debug/net6.0/A. Candies.AssemblyInfo.cs
+++ Codeforces/A. Candies/obj/Debug/net6.0/A. Candies.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("A. Candies")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9eb0e5881e9d6d03a788029952a2e143cd0c5ef1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10393059bab381478446b00a4977c027f00c68d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("A. Candies")]
 [assembly: System.Reflection.AssemblyTitleAttribute("A. Candies")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
