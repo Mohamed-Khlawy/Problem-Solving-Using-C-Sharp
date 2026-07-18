16365f4b25fb5d669a97906a5851d99d5c489bca
Khlawy <mhmdalkhlawy45@gmail.com>
2026-07-18 15:22:26 +0300

    Solved from Codeforces Problemset: A. Square Year.

---
+++ Codeforces/A Problems/Square Year.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A_Problems
+{
+    // Problem Link => https://codeforces.com/problemset/problem/2114/A
+    internal class Square_Year
+    {
+        public (bool, string) CanWeSquareYear(int year)
+        {
+            int yearSqrt = (int)Math.Sqrt(year);
+
+            if (Math.Pow(yearSqrt, 2) == year)
+            {
+                StringBuilder newRepresentation = new StringBuilder($"0 {yearSqrt}");
+                return (true, newRepresentation.ToString());
+            }
+
+            return (false, string.Empty);
+        }
+    }
+}
--- Codeforces/B. Ten Words of Wisdom/obj/Debug/net6.0/B. Ten Words of Wisdom.AssemblyInfo.cs
+++ Codeforces/B. Ten Words of Wisdom/obj/Debug/net6.0/B. Ten Words of Wisdom.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("B. Ten Words of Wisdom")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f1cfa79495279bf0b01fc1fcbda747b4bc2489c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bfcc2a3d365d6bbcc0ff268e1d6ec51b3bf1c42")]
 [assembly: System.Reflection.AssemblyProductAttribute("B. Ten Words of Wisdom")]
 [assembly: System.Reflection.AssemblyTitleAttribute("B. Ten Words of Wisdom")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
