8b4c3906b1a6cb9844a3e112185eb8368e88dc3c
Khlawy <mhmdalkhlawy45@gmail.com>
2026-08-20 23:01:57 +0300

    Visual Studio Update.

--- Codeforces/A. Insomnia cure/obj/Debug/net6.0/A. Insomnia cure.AssemblyInfo.cs
+++ Codeforces/A. Insomnia cure/obj/Debug/net6.0/A. Insomnia cure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("A. Insomnia cure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c470a90ecb94995309615b5e9d1514cf5499698b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7441886104fe88a5f597b975801f4f84c23ea551")]
 [assembly: System.Reflection.AssemblyProductAttribute("A. Insomnia cure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("A. Insomnia cure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
