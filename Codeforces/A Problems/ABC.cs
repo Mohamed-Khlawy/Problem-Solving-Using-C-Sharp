ad3592b1ce5688683f48d6c699901a8da9fbb926
Khlawy <mhmdalkhlawy45@gmail.com>
2026-09-08 22:58:28 +0300

    Solved from Codeforces Problemset: B. NIT Destroys the Universe and Added: A. ABC to A Problems Folder.

---
+++ Codeforces/A Problems/ABC.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A_Problems
+{
+    // Problem Link => https://codeforces.com/problemset/problem/1632/A
+    internal class ABC
+    {
+        public bool CanWeRemoveAllPalindromes(string binaryStr)
+        {
+            return binaryStr.Length == 1 ||
+                  (binaryStr.Length == 2 && binaryStr[0] != binaryStr[1]);
+        }
+    }
+}
---
+++ Codeforces/B Problems/NIT Destroys the Universe.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B_Problems
+{
+    // Problem Link => https://codeforces.com/problemset/problem/1696/B
+    internal class NIT_Destroys_the_Universe
+    {
+        public int NumberOfOpsToMakeAllElementsZero(int[] arr)
+        {
+            int left = 0;
+            int right = arr.Length - 1;
+            bool foundZeroBetweenNumbers = false;
+
+            while (left < right)
+            {
+                if (arr[left] == 0) left++;
+
+                if (arr[right] == 0) right--;
+
+                if (arr[left] != 0 && arr[right] != 0) break;
+            }
+
+            if (left > right) return 0;
+
+            if (left == right)
+            {
+                return arr[left] == 0 ? 0 : 1;
+            }
+
+            for (int i = left + 1; i < right; i++)
+            {
+                if (arr[i] == 0)
+                {
+                    foundZeroBetweenNumbers = true;
+                    break;
+                }
+            }
+
+            return foundZeroBetweenNumbers ? 2 : 1;
+        }
+    }
+}
